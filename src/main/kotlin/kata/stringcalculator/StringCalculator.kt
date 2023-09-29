62f023e7d64ebc90ecdf1eba8e488163ba65e898
Frank Neblung <frank.neblung@gmail.com>
2023-09-29 10:56:49 +0200

    Step 4 - Refactor
    Extract method to remove duplication

--- src/main/kotlin/kata/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/kata/stringcalculator/StringCalculator.kt
@@ -8,10 +8,13 @@ fun add(numbers: String): Int {
     val lines = numbers.lines()
     return if (lines[0].startsWith("//")) {
         val delimiter = lines[0].drop(2)
-        lines.drop(1).sumOf { addLine(delimiter, it) }
+        addLines(delimiter, lines.drop(1))
     } else {
-        lines.sumOf { addLine(",", it) }
+        addLines(",", lines)
     }
 }
+
+private fun addLines(delimiter: String, linesWithNumbers: List<String>): Int =
+    linesWithNumbers.sumOf { addLine(delimiter, it) }
 
 private fun addLine(delimiter: String, line: String) = line.split(delimiter).sumOf { it.toInt() }
