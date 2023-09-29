b70cd7c3513c20414e14eb5e637c64f877d4d3a3
Frank Neblung <frank.neblung@gmail.com>
2023-09-29 10:34:39 +0200

    Step 1 - Green bar

--- src/main/kotlin/kata/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/kata/stringcalculator/StringCalculator.kt
@@ -4,5 +4,11 @@ package kata.stringcalculator
  * See [here](https://osherove.com/tdd-kata-1)
  */
 fun add(numbers: String): Int {
-    TODO()
+    return if (numbers.isEmpty()) {
+        0
+    } else if (numbers.contains(",")) {
+        numbers.substringBefore(",").toInt() + numbers.substringAfter(",").toInt()
+    } else {
+        numbers.toInt()
+    }
 }
