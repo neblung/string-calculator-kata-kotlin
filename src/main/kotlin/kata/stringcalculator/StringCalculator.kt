cdef5658746c1c1c9852257d11084d2869eba882
Frank Neblung <frank.neblung@gmail.com>
2023-09-29 11:25:25 +0200

    Step 7 - Green bar

--- src/main/kotlin/kata/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/kata/stringcalculator/StringCalculator.kt
@@ -7,13 +7,18 @@ fun add(string: String): Int {
     if (string.isEmpty()) return 0
     val lines = string.lines()
     val numbers = if (lines[0].startsWith("//")) {
-        val delimiter = lines[0].drop(2)
+        val delimiter = delimiter(lines[0].drop(2))
         numbersOfLines(delimiter, lines.drop(1))
     } else {
         numbersOfLines(",", lines)
     }
     requireContainsNoNegatives(numbers)
     return numbers.filter { it <= 1000 }.sum()
+}
+
+private fun delimiter(definition: String): String {
+    fun dropBrackets() = definition.drop(1).dropLast(1)
+    return if (definition.startsWith("[")) dropBrackets() else definition
 }
 
 private fun numbersOfLines(delimiter: String, linesWithNumbers: List<String>): List<Int> =
