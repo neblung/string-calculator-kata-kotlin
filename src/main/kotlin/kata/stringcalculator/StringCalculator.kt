ff7b07b24cee56107a771fd31738c12a8965750c
Frank Neblung <frank.neblung@gmail.com>
2023-11-12 11:44:54 +0100

    Avoid calling delimiters.toTypedArray() repeatedly

--- src/main/kotlin/kata/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/kata/stringcalculator/StringCalculator.kt
@@ -13,28 +13,25 @@ fun add(string: String): Int {
 
 private fun getNumbers(string: String): List<Int> {
     val lines = string.lines()
-    return if (lines[0].startsWith("//")) {
-        val delimiters = delimiters(lines[0].drop(2))
-        numbersOfLines(delimiters, lines.drop(1))
+    val (delimiters, linesWithNumbers) = if (lines[0].startsWith("//")) {
+        delimiters(lines[0].drop(2)) to lines.drop(1)
     } else {
-        numbersOfLines(listOf(","), lines)
+        listOf(",") to lines
     }
+    return linesWithNumbers.collectNumbers(delimiters.toTypedArray())
 }
 
 private fun delimiters(definition: String): List<String> {
-    fun dropBrackets() = definition.drop(1).dropLast(1)
     return if (definition.startsWith("[")) {
+        fun dropBrackets() = definition.drop(1).dropLast(1)
         dropBrackets().split("][")
     } else {
         listOf(definition)
     }
 }
 
-private fun numbersOfLines(delimiters: List<String>, linesWithNumbers: List<String>): List<Int> =
-    linesWithNumbers.flatMap { numbersOfLine(delimiters, it) }
-
-private fun numbersOfLine(delimiters: List<String>, line: String) =
-    line.split(*delimiters.toTypedArray()).map { it.toInt() }
+private fun List<String>.collectNumbers(delimiters: Array<String>): List<Int> =
+    flatMap { it.split(*delimiters).map(String::toInt) }
 
 private fun List<Int>.requireContainsNoNegatives() {
     val negatives = filter { it < 0 }
